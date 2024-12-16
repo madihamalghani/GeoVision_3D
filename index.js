9a71afcada3b01d68aa07b64eb504832bd3efbf9
Madiha Fatima <144526926+madihamalghani@users.noreply.github.com>
2024-12-16 01:06:28 -0800

    geo vision

--- index.js
+++ index.js
@@ -12,12 +12,24 @@ const camera=new THREE.PerspectiveCamera(fov,aspect,near,far)
 camera.position.z=2;
 const scene=new THREE.Scene()
 const geo= new THREE.IcosahedronGeometry(1.0,2)
-const mat= new THREE.MeshBasicMaterial({color:0xccff})
+const mat= new THREE.MeshStandardMaterial({
+    color:0xffffff,
+    flatShading:true})
 const mesh=new THREE.Mesh(geo,mat)
 scene.add(mesh)
+const wireMat=new THREE.MeshBasicMaterial({
+    color:0xffffff,
+    wireframe:true
+})
+const wireMesh=new THREE.Mesh(geo,wireMat)
+wireMesh.scale.setScalar(1.001);
+mesh.add(wireMesh)
+const hemilight=new THREE.HemisphereLight(0xffffff,0x000000)
+scene.add(hemilight)
 function animate(t=0){
     requestAnimationFrame(animate)
-    mesh.scale.setScalar(Math.cos(t * 0.001)+1.0);
+    mesh.rotation.y=t * 0.0001;
+    // mesh.scale.setScalar(Math.cos(t * 0.001)+1.0);
     renderer.render(scene,camera)
 }
 animate();
