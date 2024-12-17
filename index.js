8800de5a9294bc19734352572f49846c84278905
Madiha Fatima <144526926+madihamalghani@users.noreply.github.com>
2024-12-16 23:09:12 -0800

    final 3d_vision

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 import * as THREE from "three";
 const w=window.innerWidth;
 const h=window.innerHeight;
+// The antialias: true makes the edges of shapes look smoother.
 const renderer= new THREE.WebGLRenderer({antialias:true})
 renderer.setSize(w,h);
-document.body.appendChild(renderer.domElement);
-const fov=75;
+document.body.appendChild(renderer.domElement);// Add renderer to the webpage
+const fov=75;// Field of view (how wide the camera sees)
 const aspect=w/h;
 const near=0.1;
 const far=10;
 const camera=new THREE.PerspectiveCamera(fov,aspect,near,far)
-camera.position.z=2;
-const scene=new THREE.Scene()
+camera.position.z=2;// Move the camera back so we can see the object
+const scene=new THREE.Scene()//A scene is like an empty room where you can put objects, lights, and cameras.
+const controls=new OrbitControls(camera,renderer.domElement)
+controls.enableDamping=true;
+controls.dampingFactor=0.03;
+
 const geo= new THREE.IcosahedronGeometry(1.0,2)
 const mat= new THREE.MeshStandardMaterial({
     color:0xffffff,
@@ -19,17 +25,17 @@ const mesh=new THREE.Mesh(geo,mat)
 scene.add(mesh)
 const wireMat=new THREE.MeshBasicMaterial({
     color:0xffffff,
-    wireframe:true
+    wireframe:true // Only edges of the shape are visible
 })
 const wireMesh=new THREE.Mesh(geo,wireMat)
 wireMesh.scale.setScalar(1.001);
 mesh.add(wireMesh)
-const hemilight=new THREE.HemisphereLight(0xffffff,0x000000)
+const hemilight=new THREE.HemisphereLight(0x0099ff,0x640D5F)//0x0099ff,0x00aa55
 scene.add(hemilight)
 function animate(t=0){
     requestAnimationFrame(animate)
     mesh.rotation.y=t * 0.0001;
-    // mesh.scale.setScalar(Math.cos(t * 0.001)+1.0);
     renderer.render(scene,camera)
+    controls.update();
 }
 animate();
